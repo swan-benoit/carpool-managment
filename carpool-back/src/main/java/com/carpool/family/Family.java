c3ecd9b7fd6253c306001db0177703a17dd06962
swan.benoit <swan.benoit@visiativ.com>
2025-06-22 13:58:42 +0200

    add Child entity and update Family entity to include children relationship

--- carpool-back/src/main/java/com/carpool/family/Family.java
+++ carpool-back/src/main/java/com/carpool/family/Family.java
@@ -1,10 +1,16 @@
 package com.carpool.family;
 
+import com.carpool.Child;
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+
+import java.util.List;
 
 @Entity
 public class Family extends PanacheEntity {
     public String name;
     public int carCapacity;
+    @OneToMany
+    public List<Child> children;
 }
