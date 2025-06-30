5d752e7b6d5cd6261637e3fbe1091f24a201d13b
swan.benoit <swan.benoit@visiativ.com>
2025-06-30 14:00:28 +0200

    Refactor navigation and routing components to remove unused routes and improve structure

--- carpool-front/src/app/app.routes.ts
+++ carpool-front/src/app/app.routes.ts
@@ -1,29 +1,5 @@
 import { Routes } from '@angular/router';
-import { Home } from './home/home';
-import { FamilyList } from './components/family-list/family-list';
-import { FamilyForm } from './components/family-form/family-form';
-import { Planning } from './components/planning/planning';
 
 export const routes: Routes = [
-  {
-    path: '',
-    redirectTo: '/families',
-    pathMatch: 'full'
-  },
-  {
-    path: 'families',
-    component: FamilyList
-  },
-  {
-    path: 'families/new',
-    component: FamilyForm
-  },
-  {
-    path: 'families/:id/edit',
-    component: FamilyForm
-  },
-  {
-    path: 'planning',
-    component: Planning
-  }
+
 ];
