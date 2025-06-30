8515f9b534ecd4b7978707b18ea914212bf63c92
swan.benoit <swan.benoit@visiativ.com>
2025-06-30 13:23:24 +0200

    remove ScheduleResource.java

--- carpool-back/src/main/java/com/carpool/schedule/ScheduleResource.java
+++
@@ -1,7 +0,0 @@
-package com.carpool.schedule;
-
-import com.carpool.schedule.persistence.Schedule;
-import io.quarkus.hibernate.orm.rest.data.panache.PanacheEntityResource;
-
-public interface ScheduleResource extends PanacheEntityResource<Schedule, Long> {
-}
