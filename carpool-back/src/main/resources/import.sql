3621607d85abbbef5bc8caed764a7d4e46edbe06
swan.benoit <swan.benoit@visiativ.com>
2025-07-02 15:43:35 +0200

    Add Angular Material theme and update Snackbar service integration

--- carpool-back/src/main/resources/import.sql
+++ carpool-back/src/main/resources/import.sql
@@ -4,6 +4,9 @@ INSERT INTO family (carcapacity, name) VALUES (4,  'Axel et Soizic');
 INSERT INTO family (carcapacity, name) VALUES (4,  'Abdou et Lydia');
 INSERT INTO family (carcapacity, name) VALUES (4,  'Sonia et Jean philippe');
 INSERT INTO family (carcapacity, name) VALUES (4,  'Cécile');
+INSERT INTO family (carcapacity, name) VALUES (4,  'Josephine et Mickaël');
+INSERT INTO family (carcapacity, name) VALUES (4,  'Lucie et Raphaël');
+INSERT INTO family (carcapacity, name) VALUES (4,  'Marie et Fabien');
 
 INSERT INTO child (name) VALUES ('Mael');
 INSERT INTO child (name) VALUES ( 'Luce');
@@ -11,9 +14,11 @@ INSERT INTO child (name) VALUES ( 'Come');
 INSERT INTO child (name) VALUES ( 'Rose');
 INSERT INTO child (name) VALUES ( 'Issa');
 INSERT INTO child (name) VALUES ( 'Hedi');
-INSERT INTO child (name) VALUES ( 'Laetitia');
 INSERT INTO child (name) VALUES ( 'Mélanie');
 INSERT INTO child (name) VALUES ( 'Anna');
+INSERT INTO child (name) VALUES ( 'Orion');
+INSERT INTO child (name) VALUES ( 'Marin');
+INSERT INTO child (name) VALUES ( 'Léonie');
 
 INSERT INTO family_child (family_id, children_id) VALUES ((SELECT id from family where name = 'Romain et Virginie'), (SELECT id from child where name = 'Mael'));
 INSERT INTO family_child (family_id, children_id) VALUES ((SELECT id from family where name = 'Anne et Swan'), (SELECT id from child where name = 'Luce'));
@@ -21,8 +26,20 @@ INSERT INTO family_child (family_id, children_id) VALUES ((SELECT id from family
 INSERT INTO family_child (family_id, children_id) VALUES ((SELECT id from family where name = 'Axel et Soizic'), (SELECT id from child where name = 'Rose'));
 INSERT INTO family_child (family_id, children_id) VALUES ((SELECT id from family where name = 'Abdou et Lydia'), (SELECT id from child where name = 'Hedi'));
 INSERT INTO family_child (family_id, children_id) VALUES ((SELECT id from family where name = 'Abdou et Lydia'), (SELECT id from child where name = 'Issa'));
-INSERT INTO family_child (family_id, children_id) VALUES ((SELECT id from family where name = 'Sonia et Jean philippe'), (SELECT id from child where name = 'Laetitia'));
 INSERT INTO family_child (family_id, children_id) VALUES ((SELECT id from family where name = 'Sonia et Jean philippe'), (SELECT id from child where name = 'Mélanie'));
 INSERT INTO family_child (family_id, children_id) VALUES ((SELECT id from family where name = 'Cécile'), (SELECT id from child where name = 'Anna'));
+INSERT INTO family_child (family_id, children_id) VALUES ((SELECT id from family where name = 'Josephine et Mickaël'), (SELECT id from child where name = 'Orion'));
+INSERT INTO family_child (family_id, children_id) VALUES ((SELECT id from family where name = 'Lucie et Raphaël'), (SELECT id from child where name = 'Marin'));
+INSERT INTO family_child (family_id, children_id) VALUES ((SELECT id from family where name = 'Marie et Fabien'), (SELECT id from child where name = 'Léonie'));
+
+INSERT INTO absencedays (weekday, weektype) VALUES (3, 0);
+INSERT INTO absencedays (weekday, weektype) VALUES (2, 1);
+INSERT INTO absencedays (weekday, weektype) VALUES (2, 0);
+INSERT INTO absencedays (weekday, weektype) VALUES (3, 1);
+
+INSERT INTO child_absencedays (child_id, absencedays_id) VALUES ((SELECT id from child where name = 'Anna'), (SELECT id from absencedays where weekday = 3 and weektype = 0));
+INSERT INTO child_absencedays (child_id, absencedays_id) VALUES ((SELECT id from child where name = 'Anna'), (SELECT id from absencedays where weekday = 2 and weektype = 1));
+INSERT INTO child_absencedays (child_id, absencedays_id) VALUES ((SELECT id from child where name = 'Anna'), (SELECT id from absencedays where weekday = 2 and weektype = 0));
+INSERT INTO child_absencedays (child_id, absencedays_id) VALUES ((SELECT id from child where name = 'Anna'), (SELECT id from absencedays where weekday = 3 and weektype = 1));
 
 
