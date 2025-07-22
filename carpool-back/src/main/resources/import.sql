f6c5c378fe023936b065207505788823e6f618a3
swan.benoit <swan.benoit@visiativ.com>
2025-07-22 12:51:07 +0200

    Update database initialization scripts and configuration properties

--- carpool-back/src/main/resources/import.sql
+++ carpool-back/src/main/resources/import.sql
@@ -1,45 +1,46 @@
-INSERT INTO family (carcapacity, name) VALUES (7,  'Romain et Virginie');
-INSERT INTO family (carcapacity, name) VALUES (4,  'Anne et Swan');
-INSERT INTO family (carcapacity, name) VALUES (4,  'Axel et Soizic');
-INSERT INTO family (carcapacity, name) VALUES (4,  'Abdou et Lydia');
-INSERT INTO family (carcapacity, name) VALUES (4,  'Sonia et Jean philippe');
-INSERT INTO family (carcapacity, name) VALUES (4,  'Cécile');
-INSERT INTO family (carcapacity, name) VALUES (4,  'Josephine et Mickaël');
-INSERT INTO family (carcapacity, name) VALUES (4,  'Lucie et Raphaël');
-INSERT INTO family (carcapacity, name) VALUES (4,  'Marie et Fabien');
+INSERT INTO absencedays (weekday, weektype, id) VALUES (3, 0, 1);
+INSERT INTO absencedays (weekday, weektype, id) VALUES (2, 1, 2);
+INSERT INTO absencedays (weekday, weektype, id) VALUES (2, 0, 3);
+INSERT INTO absencedays (weekday, weektype, id) VALUES (3, 1, 4);
 
-INSERT INTO child (name) VALUES ('Mael');
-INSERT INTO child (name) VALUES ( 'Luce');
-INSERT INTO child (name) VALUES ( 'Come');
-INSERT INTO child (name) VALUES ( 'Rose');
-INSERT INTO child (name) VALUES ( 'Issa');
-INSERT INTO child (name) VALUES ( 'Hedi');
-INSERT INTO child (name) VALUES ( 'Mélanie');
-INSERT INTO child (name) VALUES ( 'Anna');
-INSERT INTO child (name) VALUES ( 'Orion');
-INSERT INTO child (name) VALUES ( 'Marin');
-INSERT INTO child (name) VALUES ( 'Léonie');
+ALTER SEQUENCE absencedays_id_seq RESTART WITH 5;
 
-INSERT INTO family_child (family_id, children_id) VALUES ((SELECT id from family where name = 'Romain et Virginie'), (SELECT id from child where name = 'Mael'));
-INSERT INTO family_child (family_id, children_id) VALUES ((SELECT id from family where name = 'Anne et Swan'), (SELECT id from child where name = 'Luce'));
-INSERT INTO family_child (family_id, children_id) VALUES ((SELECT id from family where name = 'Axel et Soizic'), (SELECT id from child where name = 'Come'));
-INSERT INTO family_child (family_id, children_id) VALUES ((SELECT id from family where name = 'Axel et Soizic'), (SELECT id from child where name = 'Rose'));
-INSERT INTO family_child (family_id, children_id) VALUES ((SELECT id from family where name = 'Abdou et Lydia'), (SELECT id from child where name = 'Hedi'));
-INSERT INTO family_child (family_id, children_id) VALUES ((SELECT id from family where name = 'Abdou et Lydia'), (SELECT id from child where name = 'Issa'));
-INSERT INTO family_child (family_id, children_id) VALUES ((SELECT id from family where name = 'Sonia et Jean philippe'), (SELECT id from child where name = 'Mélanie'));
-INSERT INTO family_child (family_id, children_id) VALUES ((SELECT id from family where name = 'Cécile'), (SELECT id from child where name = 'Anna'));
-INSERT INTO family_child (family_id, children_id) VALUES ((SELECT id from family where name = 'Josephine et Mickaël'), (SELECT id from child where name = 'Orion'));
-INSERT INTO family_child (family_id, children_id) VALUES ((SELECT id from family where name = 'Lucie et Raphaël'), (SELECT id from child where name = 'Marin'));
-INSERT INTO family_child (family_id, children_id) VALUES ((SELECT id from family where name = 'Marie et Fabien'), (SELECT id from child where name = 'Léonie'));
+INSERT INTO child (id, name) VALUES (1, 'Mael');
+INSERT INTO child (id, name) VALUES (2, 'Luce');
+INSERT INTO child (id, name) VALUES (3, 'Come');
+INSERT INTO child (id, name) VALUES (4, 'Rose');
+INSERT INTO child (id, name) VALUES (5, 'Issa');
+INSERT INTO child (id, name) VALUES (6, 'Hedi');
+INSERT INTO child (id, name) VALUES (7, 'Mélanie');
+INSERT INTO child (id, name) VALUES (8, 'Anna');
+INSERT INTO child (id, name) VALUES (9, 'Orion');
+INSERT INTO child (id, name) VALUES (10, 'Marin');
 
-INSERT INTO absencedays (weekday, weektype) VALUES (3, 0);
-INSERT INTO absencedays (weekday, weektype) VALUES (2, 1);
-INSERT INTO absencedays (weekday, weektype) VALUES (2, 0);
-INSERT INTO absencedays (weekday, weektype) VALUES (3, 1);
+ALTER SEQUENCE child_id_seq RESTART WITH 11;
 
-INSERT INTO child_absencedays (child_id, absencedays_id) VALUES ((SELECT id from child where name = 'Anna'), (SELECT id from absencedays where weekday = 3 and weektype = 0));
-INSERT INTO child_absencedays (child_id, absencedays_id) VALUES ((SELECT id from child where name = 'Anna'), (SELECT id from absencedays where weekday = 2 and weektype = 1));
-INSERT INTO child_absencedays (child_id, absencedays_id) VALUES ((SELECT id from child where name = 'Anna'), (SELECT id from absencedays where weekday = 2 and weektype = 0));
-INSERT INTO child_absencedays (child_id, absencedays_id) VALUES ((SELECT id from child where name = 'Anna'), (SELECT id from absencedays where weekday = 3 and weektype = 1));
+INSERT INTO child_absencedays (child_id, absencedays_id) VALUES (8, 1);
+INSERT INTO child_absencedays (child_id, absencedays_id) VALUES (8, 2);
+INSERT INTO child_absencedays (child_id, absencedays_id) VALUES (8, 3);
+INSERT INTO child_absencedays (child_id, absencedays_id) VALUES (8, 4);
 
+INSERT INTO family (carcapacity, id, name) VALUES (7, 1, 'Romain et Virginie');
+INSERT INTO family (carcapacity, id, name) VALUES (4, 2, 'Anne et Swan');
+INSERT INTO family (carcapacity, id, name) VALUES (4, 3, 'Axel et Soizic');
+INSERT INTO family (carcapacity, id, name) VALUES (4, 4, 'Abdou et Lydia');
+INSERT INTO family (carcapacity, id, name) VALUES (4, 5, 'Sonia et Jean philippe');
+INSERT INTO family (carcapacity, id, name) VALUES (4, 6, 'Cécile');
+INSERT INTO family (carcapacity, id, name) VALUES (4, 7, 'Josephine et Mickaël');
+INSERT INTO family (carcapacity, id, name) VALUES (4, 8, 'Lucie et Raphaël');
 
+ALTER SEQUENCE family_id_seq RESTART WITH 9;
+
+INSERT INTO family_child (family_id, children_id) VALUES (1, 1);
+INSERT INTO family_child (family_id, children_id) VALUES (2, 2);
+INSERT INTO family_child (family_id, children_id) VALUES (3, 3);
+INSERT INTO family_child (family_id, children_id) VALUES (3, 4);
+INSERT INTO family_child (family_id, children_id) VALUES (4, 6);
+INSERT INTO family_child (family_id, children_id) VALUES (4, 5);
+INSERT INTO family_child (family_id, children_id) VALUES (5, 7);
+INSERT INTO family_child (family_id, children_id) VALUES (6, 8);
+INSERT INTO family_child (family_id, children_id) VALUES (7, 9);
+INSERT INTO family_child (family_id, children_id) VALUES (8, 10);
